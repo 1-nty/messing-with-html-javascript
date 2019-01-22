79fa94d9f74b277346d9e7b2d54105409e3eb856
1-nty <46821853+1-nty@users.noreply.github.com>
2019-01-22 14:39:49 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+//choose a selector with this then change it
+document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
+//or look up by id attribute
+document.getElementById("fourth").innerHTML = "Fourth element"
